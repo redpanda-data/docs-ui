3e5379af6524c2d709c4ea042ec69d3ac9eb344e
JakeSCahill <jake@redpanda.com>
2023-03-20 16:50:12 +0000

    Create layout for index pages

---
+++ src/helpers/get-page-info.js
@@ -0,0 +1,15 @@
+'use strict'
+
+module.exports = (url, { data: { root } }) => {
+  const { contentCatalog, page } = root
+  const pages = contentCatalog.findBy({ component: page.component.name, family: 'page' })
+  for (let i = 0; i < pages.length; i++) {
+    if (pages[i].pub.url === url) {
+      return {
+        title: pages[i].asciidoc.doctitle,
+        description: pages[i].asciidoc.attributes.description,
+        url: pages[i].pub.url,
+      }
+    }
+  }
+}
