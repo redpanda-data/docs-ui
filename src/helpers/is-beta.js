a8663820612ccab572ed563b39056b822068c97b
JakeSCahill <jake@redpanda.com>
2023-03-31 13:27:29 +0100

    Add feedback form modal

---
+++ src/helpers/is-beta.js
@@ -0,0 +1,11 @@
+module.exports = (currentPage) => {
+  const currentVersion = currentPage.attributes.version
+  for (let i = 0; i < currentPage.component.versions.length; i++) {
+    const version = currentPage.component.versions[i].version
+    if (currentVersion === version) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
