3e59cfd7aa34d97be0a925a7ce4f592cf8d6e68e
JakeSCahill <jake@redpanda.com>
2023-04-05 15:46:23 +0100

    Check that the page has a version

--- src/helpers/is-beta.js
+++ src/helpers/is-beta.js
@@ -1,5 +1,6 @@
 module.exports = (currentPage) => {
   const currentVersion = currentPage.attributes.version
+  if (!currentVersion) return false
   for (let i = 0; i < currentPage.component.versions.length; i++) {
     const version = currentPage.component.versions[i].version
     if (currentVersion === version) {
