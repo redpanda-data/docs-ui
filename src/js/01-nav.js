e64ae254db9e9a4338354e947db9fd49a8be0e33
Jake Cahill <45230295+JakeSCahill@users.noreply.github.com>
2023-04-12 18:23:10 +0100

    Minor fixes (#22)
    * Open nav tree item on click
    
    * Style anchor links

--- src/js/01-nav.js
+++ src/js/01-nav.js
@@ -129,6 +129,8 @@
       var menuPanelRect = menuPanel.getBoundingClientRect()
       var overflowY = (rect.bottom - menuPanelRect.top - menuPanelRect.height + padding).toFixed()
       if (overflowY > 0) menuPanel.scrollTop += Math.min((rect.top - menuPanelRect.top - padding).toFixed(), overflowY)
+      const a = this.querySelector('a')
+      a && window.open(a.href)
     }
   }
 
