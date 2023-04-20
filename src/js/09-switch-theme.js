bbf8eb74a10b2e9647f6ff598e5d5ea5f2d952e6
JakeSCahill <jake@redpanda.com>
2023-04-20 15:15:32 +0100

    Implement dark mode

---
+++ src/js/09-switch-theme.js
@@ -0,0 +1,32 @@
+;(function () {
+  'use strict'
+
+  const switchButton = document.getElementById('switch-theme')
+  if (!switchButton) return
+
+  let theme = window.localStorage.getItem('theme')
+
+  function setTheme (switchButton) {
+    console.log(theme)
+    const img = switchButton.querySelector('img:first-child')
+    if (theme === 'dark') {
+      const newSrc = img.src.replace('view-sun', 'view-moon')
+      img.src = newSrc
+      // turn it white
+      img.classList.add('moon')
+      document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+      const newSrc = img.src.replace('view-moon', 'view-sun')
+      img.src = newSrc
+      img.classList.remove('moon')
+      document.documentElement.removeAttribute('data-theme')
+    }
+  }
+
+  switchButton.addEventListener('click', function () {
+    console.log(theme)
+    theme = theme === 'light' ? 'dark' : 'light'
+    window.localStorage.setItem('theme', theme)
+    setTheme(this)
+  })
+})()
