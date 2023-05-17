aca4a2dd7ea77765c70d8d12b52e516370ad23aa
JakeSCahill <jake@redpanda.com>
2023-05-17 12:08:23 +0100

    Fix home page

--- src/helpers/get-path.js
+++ src/helpers/get-path.js
@@ -1,13 +1,10 @@
 'use strict'
 
 module.exports = (url, { data: { root } }) => {
-  console.log(url)
   const urlParts = url.split('/')
-  console.log(urlParts)
   if (urlParts[urlParts.length - 1] === '') {
     urlParts.pop() // remove trailing slash
   }
   urlParts.pop() // remove last path component
-  console.log(urlParts)
   return urlParts.join('/')
 }
