de14e6180152e338dad599522eea5389dc1d0417
JakeSCahill <jake@redpanda.com>
2023-06-05 19:18:12 +0100

    Fix tab selector

--- src/js/10-open-nested-tabs.js
+++ src/js/10-open-nested-tabs.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const tabs = document.querySelectorAll('li.tab > p')
-  tabs.forEach(function (tab) {
-    tab.addEventListener('click', function (event) {
-      const currentTab = event.target.parentElement
-      const id = currentTab.id
-      if (!id) return
-      window.location.hash = '#' + encodeURIComponent(id)
-      const panel = document.querySelector(`div#${id}--panel`)
+;(function () {
+  'use strict'
+  window.addEventListener('DOMContentLoaded', function (event) {
+    const url = new URL(window.location.href)
+    const tabId = url.searchParams.get('tab')
+    if (tabId) {
+      const decodedTabId = decodeURIComponent(tabId)
+      this.location.hash = `#${tabId}`
+      const panel = document.querySelector(`div#${decodedTabId}--panel`)
       if (panel) {
         const hiddenElement = panel.querySelector('.is-hidden[hidden]')
         if (hiddenElement) {
@@ -22,6 +22,18 @@ document.addEventListener('DOMContentLoaded', function () {
           }
         }
       }
+    }
+  })
+  const tabs = document.querySelectorAll('li.tab > p')
+  tabs.forEach(function (tab) {
+    tab.addEventListener('click', function (event) {
+      event.preventDefault()
+      const currentTab = event.target.parentElement
+      const id = currentTab.id
+      if (!id) return
+      const url = new URL(window.location.href)
+      url.searchParams.set('tab', encodeURIComponent(id))
+      window.history.pushState(null, null, url)
     })
   })
-})
+})()
