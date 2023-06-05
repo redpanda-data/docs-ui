e40133581183ea84447d403bd59eca1c55650ca9
JakeSCahill <jake@redpanda.com>
2023-06-05 17:11:20 +0100

    Fix tab selector

--- src/js/10-open-nested-tabs.js
+++ src/js/10-open-nested-tabs.js
@@ -1,27 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
   const tabs = document.querySelectorAll('li.tab')
   tabs.forEach(function (tab) {
-    tab.addEventListener('click', function () {
-      if (tab.classList.contains('is-selected')) {
-        const id = tab.id
-        console.log(id)
-        if (!id) return
-        const url = window.location.href.replace(window.location.hash, '') + '#' + encodeURIComponent(id)
-        window.history.pushState(null, null, url)
-        const panel = document.querySelector(`div#${id}--panel`)
-        if (panel) {
-          const hiddenElement = panel.querySelector('.is-hidden[hidden]')
-          if (hiddenElement) {
-            hiddenElement.classList.remove('is-hidden')
-            hiddenElement.removeAttribute('hidden')
-            hiddenElement.classList.add('is-selected')
-          }
-          const tablist = panel.querySelector('.tablist')
-          if (tablist) {
-            const firstTab = tablist.querySelector('li')
-            if (firstTab) {
-              firstTab.classList.add('is-selected')
-            }
+    tab.addEventListener('click', function (event) {
+      let currentTab = event.target
+      const id = currentTab.id
+      console.log(id)
+      if (!id) return
+      const url = window.location.href.replace(window.location.hash, '') + '#' + encodeURIComponent(id)
+      window.history.pushState(null, null, url)
+      const panel = document.querySelector(`div#${id}--panel`)
+      if (panel) {
+        const hiddenElement = panel.querySelector('.is-hidden[hidden]')
+        if (hiddenElement) {
+          hiddenElement.classList.remove('is-hidden')
+          hiddenElement.removeAttribute('hidden')
+          hiddenElement.classList.add('is-selected')
+        }
+        const tablist = panel.querySelector('.tablist')
+        if (tablist) {
+          const firstTab = tablist.querySelector('li')
+          if (firstTab) {
+            firstTab.classList.add('is-selected')
           }
         }
       }
