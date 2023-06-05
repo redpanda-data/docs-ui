fae853ab110718c907eef5f8e940ae3d0633faec
JakeSCahill <jake@redpanda.com>
2023-06-05 14:46:31 +0100

    Various UX and style improvments
    Display tab anchor on click

---
+++ src/js/10-open-nested-tabs.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const tabs = document.querySelectorAll('li.tab')
+  if (!tabs) return
+  tabs.forEach(function (tab) {
+    tab.addEventListener('click', function () {
+      if (tab.classList.contains('is-selected')) {
+        const id = tab.id
+        if (!id) return
+        const url = window.location.href.replace(window.location.hash, '') + '#' + encodeURIComponent(id)
+        window.history.pushState(null, null, url)
+        const panel = document.querySelector(`div#${id}--panel`)
+        if (panel) {
+          const hiddenElement = panel.querySelector('.is-hidden[hidden]')
+          if (hiddenElement) {
+            hiddenElement.classList.remove('is-hidden')
+            hiddenElement.removeAttribute('hidden')
+            hiddenElement.classList.add('is-selected')
+          }
+          const tablist = panel.querySelector('.tablist')
+          if (tablist) {
+            const firstTab = tablist.querySelector('li')
+            if (firstTab) {
+              firstTab.classList.add('is-selected')
+            }
+          }
+        }
+      }
+    })
+  })
+})
