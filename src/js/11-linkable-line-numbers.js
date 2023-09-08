234c9e1e3fc1d93e69fd9017e2031b342ab195c3
Jake Cahill <45230295+JakeSCahill@users.noreply.github.com>
2023-09-08 17:42:25 +0100

    feat: add support for line highlighting and line numbers in code blocks (#103)
    * feat: add support for line highlighting and line numbers in code blocks
    
    * remove example

--- src/js/11-linkable-line-numbers.js
+++ src/js/11-linkable-line-numbers.js
@@ -1,4 +1,4 @@
-;(function () {
+(function () {
   'use strict'
 
   window.addEventListener('DOMContentLoaded', function (event) {
@@ -6,21 +6,30 @@
       return 'code-' + Math.random().toString(36).substring(2, 9)
     }
 
-    var preElements = document.getElementsByTagName('pre')
+    var preElements = document.querySelectorAll('div > div > pre')
 
-    if (!preElements.length) return
-
-    for (var i = 0; i < preElements.length; i++) {
-      if (preElements[i].firstElementChild && preElements[i].firstElementChild.tagName === 'CODE') {
-        preElements[i].id = generateUniqueId()
+    preElements.forEach(function (preElem) {
+      if (preElem.firstElementChild && preElem.firstElementChild.tagName === 'CODE') {
+        preElem.id = generateUniqueId()
       }
-      var parent = preElements[i].parentElement
-      var grandparent = parent ? parent.parentElement : null
-      if (parent && parent.tagName === 'DIV' && grandparent && grandparent.tagName === 'DIV') {
-        if (grandparent.classList.contains('line-numbers')) {
-          preElements[i].classList.add('linkable-line-numbers')
+
+      var grandparent = preElem.parentElement.parentElement
+
+      // Check if the grandparent has a class that starts with 'lines'
+      Array.from(grandparent.classList).forEach(function (className) {
+        if (className.startsWith('lines')) {
+          var matches = className.match(/(\d+(-\d+)?)/g)
+          if (matches) {
+            var attributeValue = matches.join(',')
+            preElem.setAttribute('data-line', attributeValue)
+          }
         }
-      }
-    }
+        /* For future use (linkable line numbers)
+        if (className.startsWith('line-numbers')) {
+          preElem.classList.add('linkable-line-numbers')
+        }
+        */
+      })
+    })
   })
 })()
