b2f7c440afaea1ecfb8bf282a6d2c731038fbc49
JakeSCahill <jake@redpanda.com>
2023-09-20 17:27:53 +0100

    fix: Reapply line highlights when you change tabs

--- src/js/10-open-nested-tabs.js
+++ src/js/10-open-nested-tabs.js
@@ -1,38 +1,28 @@
+/* global Prism */
+/* global makePlaceholdersEditable */
+
 ;(function () {
   'use strict'
   window.addEventListener('DOMContentLoaded', function (event) {
     const url = new URL(window.location.href)
     const tabId = url.searchParams.get('tab')
     if (tabId) {
-      const decodedTabId = decodeURIComponent(tabId)
       this.location.hash = `#${tabId}`
-      const panel = document.querySelector(`div#${decodedTabId}--panel`)
-      if (panel) {
-        const hiddenElement = panel.querySelector('.is-hidden[hidden]')
-        if (hiddenElement) {
-          hiddenElement.classList.remove('is-hidden')
-          hiddenElement.removeAttribute('hidden')
-          hiddenElement.classList.add('is-selected')
-        }
-        const tablist = panel.querySelector('.tablist')
-        if (tablist) {
-          const firstTab = tablist.querySelector('li')
-          if (firstTab) {
-            firstTab.classList.add('is-selected')
-          }
-        }
-      }
     }
   })
-  const tabs = document.querySelectorAll('li.tab > p')
+  const tabs = document.querySelectorAll('li.tab')
   tabs.forEach(function (tab) {
     tab.addEventListener('click', function (event) {
       const currentTab = event.target.parentElement
       const id = currentTab.id
       if (!id) return
+      const tabContent = document.getElementById(id + '--panel')
       const url = new URL(window.location.href)
       url.searchParams.set('tab', encodeURIComponent(id))
       window.history.pushState(null, null, url)
+      // Rerun the line highlighter and editable placeholders scripts on the new tab content
+      Prism && Prism.highlightAll()
+      makePlaceholdersEditable && makePlaceholdersEditable()
     })
   })
 })()
