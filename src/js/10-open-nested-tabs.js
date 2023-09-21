a081c115a029073bf7376d4433f7a32fe14d0f26
Jake Cahill <45230295+JakeSCahill@users.noreply.github.com>
2023-09-21 11:41:44 +0100

    fix: highlights and editable placeholders in tabs (#109)

--- src/js/10-open-nested-tabs.js
+++ src/js/10-open-nested-tabs.js
@@ -13,15 +13,33 @@
   const tabs = document.querySelectorAll('li.tab')
   tabs.forEach(function (tab) {
     tab.addEventListener('click', function (event) {
-      const currentTab = event.target.parentElement
-      const id = currentTab.id
-      if (!id) return
+      const currentTab = event.target
+      let id = currentTab.id
+      if (!id) {
+        id = event.target.parentElement.id
+      }
+      const tabContent = document.getElementById(id + '--panel')
       const url = new URL(window.location.href)
       url.searchParams.set('tab', encodeURIComponent(id))
       window.history.pushState(null, null, url)
-      // Rerun the line highlighter and editable placeholders scripts on the new tab content
-      Prism && Prism.highlightAll()
-      makePlaceholdersEditable && makePlaceholdersEditable()
+      // Prism requires content to to visible
+      // so that it can calculate dimensions for the lines.
+      // Since tab content is hidden until clicked
+      // we need to rerun Prism when the tab is clicked
+      // so it can make the correct calculations.
+      // Prism also rewrites the content, so we lose editable placeholders.
+      // Rerun the script to make placeholders editable.
+      // TODO: Find a way to store editable placeholder state and reapply.
+      // If a user edits content in one tab and switches back and forth between another tab
+      // the content no longer matches the regex in editable placeholders
+      //so it is no longer editable
+      const preElementWithDataLine = tabContent.querySelector('.content pre[data-line]')
+      Promise.resolve().then(() => {
+        if (preElementWithDataLine) {
+          Prism && Prism.highlightAllUnder(tabContent)
+          makePlaceholdersEditable(tabContent)
+        }
+      })
     })
   })
 })()
