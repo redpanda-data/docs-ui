26dfa3624658565e1f6a32bbe6717e90485b2cfa
Paulo Borges <paulohtb@hotmail.com>
2023-09-27 12:51:33 -0300

    add annonuncement email bar (#107)
    * add annonuncement email bar
    
    fix lint
    
    lint
    
    lint
    
    lint
    
    lint
    
    lint
    
    * close on submit
    
    * remove wrong
    
    * fade away when submited
    
    * fix form name
    
    * change field
    
    * adjust css for mobile
    
    * adjust margin for mobile
    
    * adjust margin to some phones
    
    * add confirmation
    
    * lint
    
    * Update src/partials/announcement-email.hbs
    
    Co-authored-by: Jake Cahill <45230295+JakeSCahill@users.noreply.github.com>
    
    * change font to em
    
    * address feedback
    
    * adjust placement
    
    * Update src/partials/announcement-email.hbs
    
    Co-authored-by: Jake Cahill <45230295+JakeSCahill@users.noreply.github.com>
    
    * address feedback
    
    * fix behavior on bottom
    
    * add all under annonuncement-email container
    
    * lint
    
    * fix effect showing again after subscribe
    
    * lint
    
    * Update src/partials/announcement-email.hbs
    
    * Improve display on mobile (#118)
    
    * Improve display on mobile
    
    * adjust sizes
    
    ---------
    
    Co-authored-by: Paulo Borges <paulohtb@hotmail.com>
    
    ---------
    
    Co-authored-by: Jake Cahill <45230295+JakeSCahill@users.noreply.github.com>

---
+++ src/js/13-announcement-email.js
@@ -0,0 +1,71 @@
+(function () {
+  'use strict'
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const footer = document.querySelector('.announcement-email')
+    const closeButton = document.querySelector('.close-announcement-email-footer')
+    const submitButton = document.querySelector('.submit-announcement-button-email')
+    const inputContainer = document.querySelector('.input-container')
+    const successfulSentEmail = document.querySelector('.successful-sent-email')
+    let footerDisplayed = false
+    let footerIsStatic = false
+    let lastScrollTop = 0
+    if (typeof window !== 'undefined') {
+      if (window.localStorage.getItem('announcement-email-footer-closed') === 'true') {
+        footer.classList.add('hidden-announcement-email-footer')
+        return
+      }
+    }
+    window.addEventListener('scroll', function () {
+      const scrollTop = window.scrollY
+
+      if (scrollTop > 50 && !footerDisplayed) {
+        if (successfulSentEmail.style.display !== 'block') {
+          footer.classList.add('show-announcement-email-footer')
+          footerDisplayed = true
+        }
+      }
+
+      const scrolledToBottom = (window.innerHeight + scrollTop) >= (document.body.offsetHeight)
+
+      if (scrolledToBottom && !footerIsStatic) {
+        footer.style.position = 'static'
+        footerIsStatic = true
+        window.scrollTo(0, document.body.scrollHeight)
+      } else if (scrollTop < lastScrollTop && footerIsStatic) {
+        footer.style.position = 'fixed'
+        footerIsStatic = false
+      }
+
+      lastScrollTop = scrollTop
+    })
+
+    if (closeButton) {
+      closeButton.addEventListener('click', function () {
+        footer.classList.remove('show-announcement-email-footer')
+        footer.classList.add('hidden-announcement-email-footer')
+        footerDisplayed = false
+        footer.style.position = 'fixed'
+        footerIsStatic = false
+
+        window.localStorage.setItem('announcement-email-footer-closed', 'true')
+      })
+    }
+
+    if (submitButton) {
+      submitButton.addEventListener('click', function () {
+        inputContainer.style.display = 'none'
+        successfulSentEmail.style.display = 'block'
+        window.localStorage.setItem('announcement-email-footer-closed', 'true')
+
+        setTimeout(function () {
+          footer.classList.remove('show-announcement-email-footer')
+          footer.classList.add('hidden-announcement-email-footer')
+          footerDisplayed = false
+          footer.style.position = 'fixed'
+          footerIsStatic = false
+        }, 3000)
+      })
+    }
+  })
+})()
