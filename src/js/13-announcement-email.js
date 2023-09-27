93e3d967d4a2845fb32669807559d96de68ff794
Paulo Borges <paulohtb@hotmail.com>
2023-09-27 14:36:42 -0300

    fix close behavior

--- src/js/13-announcement-email.js
+++ src/js/13-announcement-email.js
@@ -20,7 +20,7 @@
       const scrollTop = window.scrollY
 
       if (scrollTop > 50 && !footerDisplayed) {
-        if (successfulSentEmail.style.display !== 'block') {
+        if (successfulSentEmail.style.display !== 'block' && window.localStorage.getItem('announcement-email-footer-closed') !== 'true') {
           footer.classList.add('show-announcement-email-footer')
           footerDisplayed = true
         }
