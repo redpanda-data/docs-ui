ec0d470aa5e19dee0b0c327a7f38ac23ce737f3c
JakeSCahill <jake@redpanda.com>
2023-10-23 13:51:44 +0100

    Add rapidoc PoC

--- src/js/09-switch-theme.js
+++ src/js/09-switch-theme.js
@@ -6,6 +6,9 @@
 
   let theme = window.localStorage.getItem('theme') ||
   (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+  const rapidocEl = document.getElementById('api')
+
+  setTheme(switchButton)
 
   function setTheme (switchButton) {
     const img = switchButton.querySelector('img:first-child')
@@ -16,12 +19,14 @@
       img.classList.add('moon')
       document.documentElement.setAttribute('data-theme', 'dark')
       document.body.setAttribute('data-theme', 'dark')
+      rapidocEl && rapidocEl.setAttribute('theme', 'dark')
     } else {
       const newSrc = img.src.replace('view-moon', 'view-sun')
       img.src = newSrc
       img.classList.remove('moon')
       document.documentElement.removeAttribute('data-theme')
       document.body.removeAttribute('data-theme')
+      rapidocEl && rapidocEl.setAttribute('theme', 'light')
     }
   }
 
