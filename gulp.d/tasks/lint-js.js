bfc9ac0c7301aac091073f923f95c090dcbfe23d
Paulo Borges <paulohtb@hotmail.com>
2023-11-30 15:31:15 -0300

    Modify lintjs code len, and skip vendor files

--- gulp.d/tasks/lint-js.js
+++ gulp.d/tasks/lint-js.js
@@ -2,11 +2,18 @@
 
 const eslint = require('gulp-eslint')
 const vfs = require('vinyl-fs')
+const gulpIf = require('gulp-if')
+
+const eslintConfig = {
+  rules: {
+    'max-len': ['error', { code: 200 }],
+  },
+}
 
 module.exports = (files) => (done) =>
   vfs
     .src(files)
-    .pipe(eslint())
+    .pipe(gulpIf((file) => !file.path.includes('vendor'), eslint(eslintConfig)))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
     .on('error', done)
