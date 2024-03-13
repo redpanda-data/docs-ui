74ea9d799fd4a96fd5497a0e4d4cc8f10d2b4a1b
JakeSCahill <jake@redpanda.com>
2024-03-13 16:22:18 +0000

    add support for preventing clipped tables

--- src/js/14-find-large-tables-and-code-blocks.js
+++ src/js/14-find-large-tables-and-code-blocks.js
@@ -1,10 +1,15 @@
 (function () {
   'use strict'
   document.addEventListener('DOMContentLoaded', function () {
-    const tables = document.querySelectorAll('table.tableblock')
+    const tables = document.querySelectorAll('table.tableblock:not(.no-clip)')
     const codeBlocks = document.querySelectorAll('pre')
 
     const processElement = (element, contentType) => {
+      // If the element is a table and has the "no-clip" class, return early without processing
+      if (contentType === 'table' && element.classList.contains('no-clip')) {
+        return
+      }
+
       const height = element.getBoundingClientRect().height
       if (height > 1000) {
         const wrapper = document.createElement('div')
@@ -12,8 +17,14 @@
         element.parentNode.insertBefore(wrapper, element)
         wrapper.appendChild(element)
         wrapper.style.overflow = 'scroll'
-        wrapper.style.maxHeight = '500px'
+        wrapper.style.maxHeight = '400px'
         wrapper.style.transition = 'max-height 0.5s ease'
+
+        // Create a new container for the button with flex styling
+        const buttonContainer = document.createElement('div')
+        buttonContainer.style.display = 'flex'
+        buttonContainer.style.justifyContent = 'flex-end'
+        buttonContainer.style.marginTop = '-10px'
 
         const readMoreBtn = document.createElement('button')
         readMoreBtn.innerText = contentType === 'table' ? 'Show full table' : 'View more code'
@@ -22,7 +33,11 @@
         readMoreBtn.setAttribute('role', 'button')
         readMoreBtn.setAttribute('tabindex', '0')
 
-        wrapper.parentNode.insertBefore(readMoreBtn, wrapper.nextSibling)
+        // Append the button to the new container
+        buttonContainer.appendChild(readMoreBtn)
+
+        // Insert the button container after the wrapper
+        wrapper.parentNode.insertBefore(buttonContainer, wrapper.nextSibling)
 
         const key = `expand-state-${contentType}-${Array.from(wrapper.parentNode.children).indexOf(wrapper)}`
         const storedState = window.localStorage.getItem(key)
@@ -34,7 +49,7 @@
 
         readMoreBtn.addEventListener('click', function () {
           const isExpanded = readMoreBtn.getAttribute('aria-expanded') === 'true'
-          wrapper.style.maxHeight = isExpanded ? '500px' : 'none'
+          wrapper.style.maxHeight = isExpanded ? '400px' : `${element.scrollHeight}px`
           wrapper.style.overflow = isExpanded ? 'scroll' : 'unset'
           readMoreBtn.innerText = isExpanded
             ? (contentType === 'table'
