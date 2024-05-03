f80f50bc38bb95a6c7515847981f2e5f239a913a
Jake Cahill <45230295+JakeSCahill@users.noreply.github.com>
2024-05-03 17:48:04 +0100

    Improve support for injecting API slots (#169)
    * Improve support for injecting API slots
    
    * Appease linter

--- src/helpers/get-api-slots.js
+++ src/helpers/get-api-slots.js
@@ -1,51 +1,49 @@
 'use strict'
 
-module.exports = (url, slot, { data: { root } }) => {
-  const { contentCatalog } = root
+// Keep track of processed pages
+const processedPages = new Set()
+
+module.exports = (url, { data }) => {
+  const { contentCatalog } = data.root
   if (!contentCatalog) return null
 
-  const pages = contentCatalog.findBy({ component: 'ROOT', family: 'page' })
+  const pages = contentCatalog.findBy({ component: 'api', family: 'page' })
 
-  // Find the first page that matches the 'page-api' in the URL and the 'page-api-slot' matching the slot.
-  const filteredPage = pages.find((page) => {
-    const attributes = page.asciidoc.attributes
-    return attributes['page-api'] &&
-           url.includes(attributes['page-api']) &&
-           attributes['page-api-slot'] === slot
+  // Use filter to collect all matching pages
+  const filteredPages = pages.filter((page) => {
+    if (processedPages.has(page.src.relative)) {
+      // Skip this page if it has already been processed
+      return false
+    }
+
+    const pathParts = page.src.relative.split('/')
+    if (pathParts.length !== 3) return false
+
+    // Use the first directory as the API name
+    const api = pathParts[0]
+
+    if (url.includes(api)) {
+      processedPages.add(page.src.relative)
+      return true
+    }
+    return false
+  }).map((page) => {
+    // Process each page to convert its content and wrap it with the Rapidoc slot
+    // https://rapidocweb.com/api.html#slots
+    const contentBuffer = Buffer.from(page._contents)
+    const decodedContent = contentBuffer.toString('utf8')
+    // Use the second directory as the slot name
+    return wrapHtmlWithSlot(decodedContent, page.src.relative.split('/')[1])
   })
+  // Return the array of wrapped HTML content
+  return filteredPages
+}
 
-  if (!filteredPage) return null
-
-  function removeRelativeLinkTags (htmlContent) {
-    const regex = /<a\s+(?:[^>]*?\s+)?href="(?!(http:\/\/|https:\/\/))([^"]*)"(?:[^>]*)>(.*?)<\/a>/gis
-    // Replace the matched <a> tags with just their text content
-    return htmlContent.replace(regex, '$3')
+function wrapHtmlWithSlot (htmlContent, slotName) {
+  // Check if the slotName is 'auth' or 'overview'
+  if (slotName === 'auth' || slotName === 'overview') {
+    return `<div slot="${slotName}">${htmlContent}</div>`
   }
-  function extractDocContent (htmlContent) {
-    // Regular expression to find the <article class="doc">...</article> block
-    const articleRegex = /<article\s+class="doc"[^>]*>([\s\S]*?)<\/article>/
-    const match = htmlContent.match(articleRegex)
-
-    if (!match) return ''
-
-    // Extract content inside the article
-    let articleContent = match[1]
-    // Remove unnecessary elements
-    articleContent = articleContent.replace(/<nav[^>]*>[\s\S]*?<\/nav>/g, '')
-    articleContent = articleContent.replace(/<[^>]+class="[^"]*feedback-section[^"]*"[^>]*>[\s\S]*?<\/[^>]+>/g, '')
-    articleContent = articleContent.replace(/<div[^>]*class="[^"]*col[^"]*"[^>]*>[\s\S]*?<\/div>/g, '')
-    articleContent = articleContent.replace(/<h1[^>]*>[\s\S]*?<\/h1>/g, '')
-    articleContent = articleContent.replace(/<button[^>]*class="[^"]*thumb[^"]*"[^>]*>[\s\S]*?<\/button>/g, '')
-
-    const cleanedContent = removeRelativeLinkTags(articleContent)
-
-    return cleanedContent
-  }
-  // Decode the Buffer to string for the HTML content
-  const contentBuffer = Buffer.from(filteredPage._contents)
-  const decodedContent = contentBuffer.toString('utf8')
-  const cleanedContent = extractDocContent(decodedContent)
-
-  // Return the HTML content.
-  return cleanedContent
+  // Return the content without wrapping if the slot does not match
+  return htmlContent
 }
