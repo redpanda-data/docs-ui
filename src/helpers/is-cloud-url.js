0ce0f2d1ead767e8b85a20c14505526cc26d2b29
Paulo Borges <paulohtb@hotmail.com>
2024-05-16 09:48:14 -0300

    modify helper function

--- src/helpers/is-cloud-url.js
+++ src/helpers/is-cloud-url.js
@@ -1,19 +1,8 @@
 'use strict'
-module.exports = (navUrl, { data: { root } }) => {
-  const { contentCatalog, page } = root
-  if (page.layout === '404') return
+module.exports = (url, { data: { root } }) => {
+  const { contentCatalog } = root
+  console.log(url)
   if (contentCatalog === undefined) return
-  var pages = contentCatalog.navGroup
-  var cp = contentCatalog.cp
-  if (!pages || cp !== page.component.name) {
-    pages = contentCatalog.findBy({ component: page.component.name, family: 'page' })
-    contentCatalog.navGroup = pages
-    contentCatalog.cp = page.component.name
-  }
-  for (let i = 0; i < pages.length; i++) {
-    if (pages[i].pub.url === navUrl &&
-      pages[i].asciidoc.attributes['page-cloud-api']) {
-      return true
-    }
-  }
+  if (!url.includes('cloud')) return false
+  return true
 }
