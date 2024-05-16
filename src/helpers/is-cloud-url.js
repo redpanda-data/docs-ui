b1f47403aa982c83a9ab2ff70298b75d32a2fe60
Paulo Borges <paulohtb@hotmail.com>
2024-05-16 09:04:50 -0300

    modify to use page attributes

--- src/helpers/is-cloud-url.js
+++ src/helpers/is-cloud-url.js
@@ -1,7 +1,19 @@
 'use strict'
 module.exports = function (context) {
-  const pageUrl = context.getFile().getPath()
-  const searchString = 'cloud'
-
-  return pageUrl.includes(searchString)
+  const { contentCatalog, page } = root
+  if (page.layout === '404') return
+  if (contentCatalog === undefined) return
+  var pages = contentCatalog.navGroup
+  var cp = contentCatalog.cp
+  if (!pages || cp !== page.component.name) {
+    pages = contentCatalog.findBy({ component: page.component.name, family: 'page' })
+    contentCatalog.navGroup = pages
+    contentCatalog.cp = page.component.name
+  }
+  for (let i = 0; i < pages.length; i++) {
+    if (pages[i].pub.url === navUrl &&
+      pages[i].asciidoc.attributes['page-cloudapi']) {
+      return true
+    }
+  }
 }
