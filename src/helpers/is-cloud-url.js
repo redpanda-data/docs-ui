4b880f9b6e9c734634d19f1819a9856ceaaf4637
Paulo Borges <paulohtb@hotmail.com>
2024-05-16 09:11:03 -0300

    Update src/helpers/is-cloud-url.js

--- src/helpers/is-cloud-url.js
+++ src/helpers/is-cloud-url.js
@@ -12,7 +12,7 @@ module.exports = (navUrl, { data: { root } }) => {
   }
   for (let i = 0; i < pages.length; i++) {
     if (pages[i].pub.url === navUrl &&
-      pages[i].asciidoc.attributes['page-cloudapi']) {
+      pages[i].asciidoc.attributes['page-cloud-api']) {
       return true
     }
   }
