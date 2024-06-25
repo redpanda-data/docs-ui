9fded06c3499ddbba1e7328bb941594e18b70635
JakeSCahill <jake@redpanda.com>
2024-06-25 12:39:35 +0100

    Allow for pages in docs to list related labs

---
+++ src/helpers/list-related-labs.js
@@ -0,0 +1,23 @@
+'use strict'
+
+module.exports = (title, attributes, content, { data: { root } }) => {
+  const { contentCatalog } = root
+  if (attributes['component-name'] === 'redpanda-labs') return content
+  if (!contentCatalog) return content
+
+  // Extract related labs from attributes
+  const relatedLabs = attributes['related-labs'] ? JSON.parse(attributes['related-labs']) : []
+  if (!relatedLabs.length) return content
+
+  // Append "Suggested labs" heading if there are matching labs
+  let contentString = content.toString('utf8')
+  contentString += '<div class="ulist">\n<ul>'
+
+  relatedLabs.forEach((lab, index) => {
+    contentString += `<li><a href="${lab.url}">${lab.title}</a>: ${lab.description}</li>`
+  })
+  contentString += '</ul></div>'.trim()
+
+  const modifiedContent = Buffer.from(contentString, 'utf8')
+  return modifiedContent
+}
