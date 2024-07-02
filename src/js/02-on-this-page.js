5183f967e009e576c9c1f8a7f8f7f61cb0313bd9
JakeSCahill <jake@redpanda.com>
2024-07-02 19:26:49 +0100

    Stop click event from bubbling up

--- src/js/02-on-this-page.js
+++ src/js/02-on-this-page.js
@@ -81,6 +81,7 @@
     tocMenuDropdown.querySelectorAll('.toc-menu a').forEach(function (link) {
       link.addEventListener('click', function (e) {
         e.preventDefault()
+        e.stopPropagation()
         var targetId = this.getAttribute('href').substring(1)
         var targetElement = document.getElementById(targetId)
         if (targetElement) {
@@ -91,7 +92,7 @@
           })
         }
         // Hide the dropdown menu after click
-        tocMenu.classList.add('hidden')
+        tocMenu.classList.toggle('hidden')
       })
     })
   }
