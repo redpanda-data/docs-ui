07fc69a9bcad99a3bbb05773423ee270178b5371
JakeSCahill <jake@redpanda.com>
2024-07-15 15:26:56 +0100

    Fix announcement bar:

--- src/js/12-announcement-banner.js
+++ src/js/12-announcement-banner.js
@@ -3,19 +3,13 @@
   window.addEventListener('DOMContentLoaded', function (event) {
     var announcementBar = document.querySelector('.announcement-bar')
     if (!announcementBar) return
-    var navbarBrand = document.getElementById('brand')
     var closeButton = document.getElementById('close-announcement')
     // Hide the announcement bar if the session cookie is set
     if (window.sessionStorage.getItem('announcementClosed')) {
       announcementBar.style.display = 'none'
-      navbarBrand.style.height = 'inherit'
-    } else {
-      // When the announcement bar is not closed, reset height
-      navbarBrand.style.height = '70px'
     }
     closeButton.addEventListener('click', function () {
       announcementBar.style.display = 'none'
-      navbarBrand.style.height = 'inherit'
       window.sessionStorage.setItem('announcementClosed', 'true')
     })
   })
