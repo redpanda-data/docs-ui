a560ae411d23eae1dc8bd8b3510a784259ece2aa
JakeSCahill <jake@redpanda.com>
2024-12-12 10:19:15 +0000

    Support filtering by component and version

--- src/helpers/get-index-data.js
+++ src/helpers/get-index-data.js
@@ -1,17 +1,40 @@
-
 module.exports = ({ data: { root } }) => {
   const { page } = root
   const indexKey = page.attributes['index-data']
   if (!indexKey) return null
+
   const indexData = page.component.asciidoc.attributes[indexKey]
   if (!indexData) return null
+  const matchComponentVersion = page.attributes['match-component-version']
+
   try {
     const parsedData = JSON.parse(indexData)
     const currentPageUrl = page.url
-    const filteredData = parsedData.filter((item) => item.url !== currentPageUrl)
+    const currentComponent = page.component.name
+    const currentVersion = page.version
+
+    const filteredData = parsedData.reduce((acc, item) => {
+      if (item.pages && Array.isArray(item.pages)) {
+        const filteredPages = item.pages.filter((page) => {
+          const isNotCurrentPage = page.url !== currentPageUrl
+
+          // Optional filtering by component and version
+          const matchesComponentVersion = !matchComponentVersion || (
+            item.component === currentComponent && item.version === currentVersion
+          )
+
+          return isNotCurrentPage && matchesComponentVersion
+        })
+        acc.push(...filteredPages)
+      }
+      return acc
+    }, [])
+
     return filteredData
   } catch (error) {
-    console.log(`Error parsing JSON attribute "${indexKey}" in ${page.url} component. Index page will be empty. Make sure the generate-index-data extension is correctly configured.\n\n ${error}`)
+    console.log(
+      `Error parsing JSON attribute "${indexKey}" in ${page.url} component. Index page will be empty. Make sure the generate-index-data extension is correctly configured.\n\n ${error}`
+    )
     return null
   }
 }
