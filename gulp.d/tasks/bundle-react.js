8c0f6b8595d06892124497f6f300c753896d00c3
JakeSCahill <jake@redpanda.com>
2025-07-06 09:15:01 +0100

    Use esbuild and browserify

--- gulp.d/tasks/bundle-react.js
+++ gulp.d/tasks/bundle-react.js
@@ -1,44 +1,65 @@
 'use strict'
 
-const browserify = require('browserify')
-const babelify = require('babelify')
-const source = require('vinyl-source-stream')
-const buffer = require('vinyl-buffer')
-const sourcemaps = require('gulp-sourcemaps')
+const esbuild = require('esbuild')
 const glob = require('glob')
-const merge = require('merge-stream')
 const path = require('path')
 const log = require('fancy-log')
-const { dest } = require('gulp')
+const fs = require('fs')
 
-function bundleAllReactTask ({ srcDir, destDir }) {
+/**
+ * Bundles all React components using esbuild
+ *
+ * @param {Object} options - Configuration options
+ * @param {string} options.srcDir - Directory containing React components
+ * @param {string} options.destDir - Output directory for bundled files
+ * @returns {Promise} A promise that resolves when bundling is complete
+ */
+async function bundleAllReactTask ({ srcDir, destDir }) {
+  // Find all JS and JSX files in the source directory
   const entries = glob.sync(path.join(srcDir, '**/*.{js,jsx}'))
+
   if (entries.length === 0) {
     log.warn(`No React modules found in ${srcDir}`)
     return Promise.resolve()
   }
 
-  const tasks = entries.map((entryFile) => {
+  // Make sure destination directory exists
+  if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(destDir, { recursive: true })
+  }
+
+  // Bundle each entry file separately
+  const bundlePromises = entries.map(async (entryFile) => {
     const base = path.basename(entryFile).replace(/\.(jsx|js)$/, '')
     const outName = `${base}.bundle.js`
-    return browserify({ entries: entryFile, debug: true })
-      .transform(babelify, {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
-        sourceMaps: true,
+    const outPath = path.join(destDir, outName)
+
+    try {
+      // Build with esbuild
+      await esbuild.build({
+        entryPoints: [entryFile],
+        bundle: true,
+        minify: process.env.NODE_ENV === 'production',
+        sourcemap: true,
+        outfile: outPath,
+        format: 'iife', // Immediately Invoked Function Expression format
+        target: ['es2020'],
+        platform: 'browser',
+        loader: {
+          '.js': 'jsx', // Treat .js files as JSX too
+          '.jsx': 'jsx',
+        },
+        define: {
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+        },
       })
-      .bundle()
-      .on('error', function (err) {
-        log.error(err)
-        this.emit('end')
-      })
-      .pipe(source(outName))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write('.'))
-      .pipe(dest(destDir))
+      log.info(`Built ${outName}`)
+    } catch (error) {
+      log.error(`Error building ${base}:`, error.message)
+    }
   })
 
-  return merge(tasks)
+  return Promise.all(bundlePromises)
 }
 
 module.exports = ({ srcDir, destDir }) => () => bundleAllReactTask({ srcDir, destDir })
